586090ce5d774fa3f2e77afae07552a85058521e
Somjintana Korbut <nuisomjin@gmail.com>
2019-09-10 09:37:54 +0700

    CA-1473 Implement side menu

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -1,42 +1,21 @@
 <template>
   <div id="wrapper">
     <navigation></navigation>
-    <img id="logo" src="~@/assets/logo.png" alt="electron-vue">
-    <main>
-      <div class="left-side">
-        <span class="title">
-          Welcome to your new project!
-        </span>
-        <system-information></system-information>
-      </div>
-
-      <div class="right-side">
-        <div class="doc">
-          <div class="title">Getting Started</div>
-          <p>
-            electron-vue comes packed with detailed documentation that covers everything from
-            internal configurations, using the project structure, building your application,
-            and so much more.
-          </p>
-          <button @click="open('https://simulatedgreg.gitbooks.io/electron-vue/content/')">Read the Docs</button><br><br>
-        </div>
-        <div class="doc">
-          <div class="title alt">Other Documentation</div>
-          <button class="alt" @click="open('https://electron.atom.io/docs/')">Electron</button>
-          <button class="alt" @click="open('https://vuejs.org/v2/guide/')">Vue.js</button>
-        </div>
-      </div>
-    </main>
+    <section class="main-content columns is-mobile">
+      <side-navigation></side-navigation>
+      <div class="column  is-desktop">xx</div>
+    </section>
   </div>
 </template>
 
 <script>
   import SystemInformation from './LandingPage/SystemInformation'
   import Navigation from './Navigation/Navigation'
+  import SideNavigation from './SideNavigation/SideNavigation'
 
   export default {
     name: 'landing-page',
-    components: { SystemInformation, Navigation },
+    components: { SystemInformation, Navigation, SideNavigation },
     methods: {
       open (link) {
         this.$electron.shell.openExternal(link)
@@ -51,7 +30,7 @@
   body { font-family: 'Source Sans Pro', sans-serif; }
 
   .navbar {
-      padding: 0.5rem;
+      padding: 0 0.75rem;
   }
 
   .user-info-nav {
@@ -71,75 +50,57 @@
   }
 
   #wrapper {
-    background:
-      radial-gradient(
-        ellipse at top left,
-        rgba(255, 255, 255, 1) 40%,
-        rgba(229, 229, 229, .9) 100%
-      );
+    background-color: #FDFDFD;
     height: 100vh;
     padding: 0;
     width: 100vw;
   }
 
-  #logo {
-    height: auto;
-    margin-bottom: 20px;
-    width: 420px;
+  .main-content {
+    padding: 0 0.75rem;
   }
 
-  main {
+  .menu {
+    background-color: #fff;
+  }
+
+  .menu-container {
     display: flex;
+    flex-direction: row;
     justify-content: space-between;
   }
 
-  main > div { flex-basis: 50%; }
-
-  .left-side {
-    display: flex;
-    flex-direction: column;
+  .menu-label {
+    margin: auto 0 !important;
   }
 
-  .welcome {
-    color: #555;
-    font-size: 23px;
-    margin-bottom: 10px;
+  .menu-list a {
+    border-radius: 0;
   }
 
-  .title {
-    color: #2c3e50;
-    font-size: 20px;
-    font-weight: bold;
-    margin-bottom: 6px;
-  }
-
-  .title.alt {
-    font-size: 18px;
-    margin-bottom: 10px;
-  }
-
-  .doc p {
-    color: black;
-    margin-bottom: 10px;
-  }
-
-  .doc button {
-    font-size: .8em;
-    cursor: pointer;
-    outline: none;
-    padding: 0.75em 2em;
-    border-radius: 2em;
-    display: inline-block;
-    color: #fff;
-    background-color: #4fc08d;
-    transition: all 0.15s ease;
-    box-sizing: border-box;
-    border: 1px solid #4fc08d;
-  }
-
-  .doc button.alt {
-    color: #42b983;
+  .menu a.is-active {
+    border-left: 5px solid #2FB04A;
     background-color: transparent;
+    color: #4a4a4a;
+    font-weight: bold;
+  }
+
+  .menu .menu-item {
+    padding: 0.5em 0 0.5em 0.75em;
+  }
+
+  .menu div.is-active {
+    border-left: 5px solid #2FB04A;
+    background-color: transparent;
+  }
+
+  .menu div.is-active .stream-title {
+    font-weight: bold;
+  }
+
+  .progress {
+    margin: 0.5rem auto !important;
+    height: 0.5rem;
   }
 
   @media screen and (min-width: 400px) {
@@ -157,6 +118,10 @@
     .navbar-item {
       display: flex;
     }
+
+    /* .columns.is-desktop {
+      display: flex;
+    } */
 
   }
 
---
+++ src/renderer/components/SideNavigation/SideNavigation.vue
@@ -0,0 +1,50 @@
+<template>
+    <aside class="column is-one-quarter menu">
+        <div class="menu-container">
+            <p class="menu-label"> {{ menuTitle }} </p>
+            <a href="#"><img src="~@/assets/ic-add.svg"></a>
+        </div>
+        <ul class="menu-list">
+            <li v-for="item in items" :key="item.id">
+                <div class="menu-item" :class="item.isActive">
+                    <div class="menu-container">
+                        <span class="stream-title"> {{ item.id }} </span>
+                        <img :src="getStateImgUrl(item.state)">
+                    </div>
+                    <div class="state-progress" v-if="item.notSynced">
+                        <progress class="progress is-primary" :value="item.progress" max="100"></progress>
+                        <div class="menu-container">
+                            <span>{{ item.state }}</span>
+                            <span> {{ item.additional }} </span>
+                        </div>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </aside>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        menuTitle: 'Streams',
+        siteName: 'Osa Conservation',
+        login: true,
+        items: [
+          { id: 'Jaguar 1', state: 'uploading', progress: 60, additional: '2/4 synced 1 error', notSynced: true, isActive: true },
+          { id: 'Pucho Tree', state: 'waiting', progress: 0, additional: '', notSynced: true, isActive: false },
+          { id: 'Jaguar 2', state: 'completed', progress: 100, additional: '', notSynced: false, isActive: false }
+        ],
+        isActive: {
+          'is-active': true
+        }
+      }
+    },
+    methods: {
+      getStateImgUrl (state) {
+        return require(`../../assets/ic-state-${state}.svg`)
+      }
+    }
+  }
+</script>
