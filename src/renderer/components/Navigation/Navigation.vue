7b8e5c2e883f21f9bf4f54636c454a39851e896e
Somjintana Korbut <nuisomjin@gmail.com>
2019-09-11 13:48:36 +0700

    Add action in navbar to go to landing page

--- src/renderer/components/Navigation/Navigation.vue
+++ src/renderer/components/Navigation/Navigation.vue
@@ -2,7 +2,7 @@
   <nav class="navbar" role="navigation" aria-label="main navigation">
   <div class="navbar-brand">
     <div class="navbar-item">
-      <img src="~@/assets/rfcx-logo.png" alt="rfcx">
+      <router-link to="/"><img src="~@/assets/rfcx-logo.png" alt="rfcx"></router-link>
     </div>
     <span class="navbar-item">RFCx Ingestion</span>
   </div>
