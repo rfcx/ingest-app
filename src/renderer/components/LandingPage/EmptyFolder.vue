6b0340c7b5e989c6c285f75f6b55db0d014a77d0
Somjintana Korbut <nuisomjin@gmail.com>
2019-09-17 19:35:30 +0700

    CSS - change margin of the empty views

--- src/renderer/components/LandingPage/EmptyFolder.vue
+++ src/renderer/components/LandingPage/EmptyFolder.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container-box empty has-text-centered">
-        <img src="~@/assets/ic-folder-empty.svg" style="margin-bottom: 8px"><br>
+        <img src="~@/assets/ic-folder-empty.svg" style="margin-bottom: 0.75em"><br>
         <span>Your synced folder is empty</span><br>
-        <a class="button is-rounded is-primary" style="margin-top: 8px">Open Folder</a>
+        <a class="button is-rounded is-primary" style="margin-top: 0.75em">Open Folder</a>
     </div>
 </template>
--- src/renderer/components/LandingPage/EmptyStream.vue
+++ src/renderer/components/LandingPage/EmptyStream.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container-box empty has-text-centered">
-        <img src="~@/assets/ic-folder.svg" style="margin-bottom: 8px"><br>
+        <img src="~@/assets/ic-folder.svg" style="margin-bottom: 0.75em"><br>
         <span>Drop folders here to create new streams or use </span>
         <img src="~@/assets/ic-add.svg"><span> to create one.</span><br>
     </div>
