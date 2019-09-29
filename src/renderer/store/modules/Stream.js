4afaf5dfad9df69de3afdfe4f1833a6b1ee026f8
Somjintana Korbut <nuisomjin@gmail.com>
2019-09-29 08:54:40 +0700

    CA-1554 Add streams to Vuex

--- src/renderer/store/modules/Stream.js
+++ src/renderer/store/modules/Stream.js
@@ -1,11 +1,15 @@
 const state = {
-  selectedStream: {}
+  selectedStream: {},
+  streams: []
 }
 
 const actions = {
   SET_SELECTED_STREAM ({ commit }, stream) {
     console.log(stream)
     commit('SET_SELECTED_STREAM', stream)
+  },
+  ADD_STREAM ({ commit }, stream) {
+    commit('ADD_STREAM', stream)
   }
 }
 
@@ -13,6 +17,9 @@ const mutations = {
   SET_SELECTED_STREAM (state, stream) {
     console.log('SET_SELECTED_STREAM')
     state.selectedStream = stream
+  },
+  ADD_STREAM (state, stream) {
+    state.streams.push(stream)
   }
 }
 
