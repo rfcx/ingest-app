0c72ab9618c1053bc31fc12e3b7434b07496c10a
Somjintana Korbut <nuisomjin@gmail.com>
2019-10-03 17:09:30 +0700

    CA-1553 Refactor file helper

--- utils/fileHelper.js
+++ utils/fileHelper.js
@@ -2,22 +2,41 @@ const fs = require('fs')
 const path = require('path')
 const cryptoJS = require('crypto-js')
 
+function getFilePath (directoryPath, fileName) {
+  return path.join(directoryPath, fileName)
+}
+
+function isFile (filePath) {
+  return fs.lstatSync(filePath).isFile()
+}
+
+function readFile (filePath) {
+  return fs.readFileSync(filePath)
+}
+
+function getExtension (fileName) {
+  return fileName.split('.').pop()
+}
+
 module.exports = {
   getFilesFromDirectoryPath: (directoryPath) => {
     return fs.readdirSync(directoryPath)
   },
   readFile: (directoryPath, fileName) => {
-    return fs.readFileSync(path.join(directoryPath, fileName))
+    const filePath = getFilePath(directoryPath, fileName)
+    return readFile(filePath)
   },
   getMD5Hash: (directoryPath, fileName) => {
-    const fileData = fs.readFileSync(path.join(directoryPath, fileName))
+    const filePath = getFilePath(directoryPath, fileName)
+    if (!isFile(filePath)) return ''
+    const fileData = readFile(filePath)
     return cryptoJS.MD5(fileData).toString()
   },
   getFileName: (fileName) => {
-    const ext = '.' + fileName.split('.').pop() // FIXME: call getExtension function
+    const ext = '.' + getExtension(fileName) // FIXME: call getExtension function
     return fileName.replace(ext, '')
   },
   getExtension: (fileName) => {
-    return fileName.split('.').pop()
+    return getExtension(fileName)
   }
 }
