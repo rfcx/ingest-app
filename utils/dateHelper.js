158c33d1086530717507dca7bd94b30e057e5e0f
Somjintana Korbut <nuisomjin@gmail.com>
2019-10-28 13:39:24 +0700

    Edit date helper to support 2 digit year

--- utils/dateHelper.js
+++ utils/dateHelper.js
@@ -3,31 +3,37 @@ const moment = require('moment')
 const appDate = 'YYYY-MM-DD HH:mm:ss'
 
 const debugLog = false
-function print (any) {
+function log (any) {
   if (debugLog) console.log(any)
 }
 module.exports = {
   appDate: appDate,
   getDateTime: (fileName, timeStampFormat) => {
+    print('timestamp format: ' + timeStampFormat)
+    timeStampFormat = timeStampFormat.replace(/%/g, '')
     print('filename: ' + fileName + ' timestamp format: ' + timeStampFormat)
-    const stringOffsetYear = timeStampFormat.search(/Y/)
+    const stringOffsetYear4Digits = timeStampFormat.search(/Y/)
+    const stringOffsetYear2Digits = timeStampFormat.search(/y/)
     const stringOffsetMonth = timeStampFormat.search(/M/)
     const stringOffsetDay = timeStampFormat.search(/D/)
     const stringOffsetHour = timeStampFormat.search(/H/)
     const stringOffsetMin = timeStampFormat.search(/m/)
     const stringOffsetSec = timeStampFormat.search(/s/)
-    const year = fileName.substr(stringOffsetYear, 4)
+    const year4Digits = fileName.substr(stringOffsetYear4Digits, 4)
+    const year2Digits = fileName.substr(stringOffsetYear2Digits, 2)
+    const year = (stringOffsetYear4Digits !== -1) ? year4Digits : '20' + year2Digits // FIXME: refactor this
     const month = fileName.substr(stringOffsetMonth, 2)
     const day = fileName.substr(stringOffsetDay, 2)
     const hour = fileName.substr(stringOffsetHour, 2)
     const min = fileName.substr(stringOffsetMin, 2)
     const sec = fileName.substr(stringOffsetSec, 2)
-    print(stringOffsetYear + '=' + year)
-    print(stringOffsetMonth + '=' + month)
-    print(stringOffsetDay + '=' + day)
-    print(stringOffsetHour + '=' + hour)
-    print(stringOffsetMin + '=' + min)
-    print(stringOffsetSec + '=' + sec)
+    log(stringOffsetYear4Digits + '=' + year4Digits)
+    log(stringOffsetYear2Digits + '=' + year2Digits)
+    log(stringOffsetMonth + '=' + month)
+    log(stringOffsetDay + '=' + day)
+    log(stringOffsetHour + '=' + hour)
+    log(stringOffsetMin + '=' + min)
+    log(stringOffsetSec + '=' + sec)
 
     // add milli sec
     const milliSeconds = 0
@@ -36,7 +42,7 @@ module.exports = {
     const timezoneOffset = '+0000'
 
     const dateTimeISO = year + '-' + month + '-' + day + 'T' + hour + ':' + min + ':' + sec + '.' + milliSeconds + timezoneOffset
-    print('date iso: ' + dateTimeISO)
+    log('date iso: ' + dateTimeISO)
     return dateTimeISO
   },
   getMomentDateFromISODate: (date) => {
