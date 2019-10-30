2b178ac5289a284526d8f19ec040bc347e39978b
Somjintana Korbut <nuisomjin@gmail.com>
2019-10-30 22:02:15 +0700

    Refactor helper files

--- utils/fileHelper.js
+++ utils/fileHelper.js
@@ -2,55 +2,61 @@ const fs = require('fs')
 const path = require('path')
 const cryptoJS = require('crypto-js')
 
-function getFilePath (directoryPath, fileName) {
+const getFilePath = (directoryPath, fileName) => {
   return path.join(directoryPath, fileName)
 }
 
-function isFile (filePath) {
+const isFile = (filePath) => {
   return fs.lstatSync(filePath).isFile()
 }
 
-function readFile (filePath) {
+const readFile = (filePath) => {
   return fs.readFileSync(filePath)
 }
 
-function getExtension (fileName) {
+const getExtension = (fileName) => {
   return fileName.split('.').pop()
 }
 
-module.exports = {
-  getFilesFromDirectoryPath: (directoryPath) => {
-    if (fs.existsSync(directoryPath)) {
-      return fs.readdirSync(directoryPath)
-    }
-    return undefined
-  },
-  readFile: (filePath) => {
-    return readFile(filePath)
-  },
-  getMD5Hash: (filePath) => {
-    if (!isFile(filePath)) return ''
-    const fileData = readFile(filePath)
-    return cryptoJS.MD5(fileData).toString()
-  },
-  getFilePath (directoryPath, fileName) {
-    return getFilePath(directoryPath, fileName)
-  },
-  getFileNameFromFilePath (filePath) {
-    return path.parse(filePath).base
-  },
-  getFileName: (fileName) => {
-    const ext = '.' + getExtension(fileName) // FIXME: call getExtension function
-    return fileName.replace(ext, '')
-  },
-  getExtension: (fileName) => {
-    return getExtension(fileName)
-  },
-  getFileSize: (filePath) => {
-    const stats = fs.statSync(filePath)
-    return stats.size
-  },
-  isSupportedFileExtension: (fileExtension) => {
-    return fileExtension === 'wav' || fileExtension === 'mp3' || fileExtension === 'opus' || fileExtension === 'flac'
+const getFilesFromDirectoryPath = (directoryPath) => {
+  if (fs.existsSync(directoryPath)) {
+    return fs.readdirSync(directoryPath)
   }
+  return undefined
+}
+
+const getMD5Hash = (filePath) => {
+  if (!isFile(filePath)) return ''
+  const fileData = readFile(filePath)
+  return cryptoJS.MD5(fileData).toString()
+}
+
+const getFileNameFromFilePath = (filePath) => {
+  return path.parse(filePath).base
+}
+
+const getFileName = (fileName) => {
+  const ext = '.' + getExtension(fileName) // FIXME: call getExtension function
+  return fileName.replace(ext, '')
+}
+
+const getFileSize = (filePath) => {
+  const stats = fs.statSync(filePath)
+  return stats.size
+}
+
+const isSupportedFileExtension = (fileExtension) => {
+  return fileExtension === 'wav' || fileExtension === 'mp3' || fileExtension === 'opus' || fileExtension === 'flac'
+}
+
+export default {
+  getFilesFromDirectoryPath,
+  readFile,
+  getMD5Hash,
+  getFilePath,
+  getFileNameFromFilePath,
+  getFileName,
+  getExtension,
+  getFileSize,
+  isSupportedFileExtension
 }
--- utils/fileWatcher.js
+++ utils/fileWatcher.js
@@ -2,16 +2,17 @@ const chokidar = require('chokidar')
 
 let watcher
 
-module.exports = {
-  watch: (path, addCallback, removeCallback) => {
-    console.log('--> ', path)
-    watcher = chokidar.watch(path, {
-      ignored: /(^|[/\\])\../, // ignore dotfiles
-      persistent: true,
-      awaitWriteFinish: true
-    })
-    watcher
-      .on('add', path => addCallback(path))
-      .on('unlink', path => removeCallback(path))
-  }
+const watch = (path, addCallback, removeCallback) => {
+  console.log('--> ', path)
+  watcher = chokidar.watch(path, {
+    ignored: /(^|[/\\])\../, // ignore dotfiles
+    persistent: true,
+    awaitWriteFinish: true
+  })
+  watcher
+    .on('add', path => addCallback(path))
+    .on('unlink', path => removeCallback(path))
+}
+export default {
+  watch
 }
