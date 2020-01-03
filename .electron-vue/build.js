931c6fe2f848ad04bb992dfe2afa58e1906c5f4b
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2020-01-03 23:17:55 +0300

    change app icon size

--- .electron-vue/build.js
+++ .electron-vue/build.js
@@ -24,7 +24,7 @@ else if (process.env.BUILD_TARGET === 'web') web()
 else build()
 
 function clean () {
-  del.sync(['build/*', '!build/icons', '!build/icons/icon.*', '!build/icons/ingest.*'])
+  del.sync(['build/*', '!build/icons', '!build/icons/icon.*', '!build/icons/ingest-mac.*', '!build/icons/ingest-win.*'])
   console.log(`\n${doneLog}\n`)
   process.exit()
 }
