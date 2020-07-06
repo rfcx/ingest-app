69f12cfaeaf7f437e3c96a0afea2f8d4fd991224
Somjintana Korbut <nuisomjin@gmail.com>
2020-07-06 17:13:31 +0700

    WB-1714 Add scope to css

--- src/renderer/components/SideNavigation/GlobalProgress.vue
+++ src/renderer/components/SideNavigation/GlobalProgress.vue
@@ -53,7 +53,7 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
   .global-progress {
     &__content-wrapper {
       display: flex;
--- src/renderer/components/SideNavigation/SideNavigation.vue
+++ src/renderer/components/SideNavigation/SideNavigation.vue
@@ -450,7 +450,7 @@
   }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 
   .header {
     padding: 0 8px 0 16px;
