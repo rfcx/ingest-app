682373f987a02b29186179d4988b9d6e2aff0f3d
Somjintana Korbut <nuisomjin@gmail.com>
2020-07-10 16:57:08 +0700

    WB-1560 setup linting tool

--- src/renderer/components/CreateStream/CreateStreamPage.vue
+++ src/renderer/components/CreateStream/CreateStreamPage.vue
@@ -1,28 +1,36 @@
 <template>
   <fieldset class="fieldset__wrapper">
-  <div class="notification" v-show="error">
-    <button class="delete" @click="onCloseAlert()"></button>
-    {{ error }}
-  </div>
-  <div class="field field-stream-name">
-    <label for="name" class="label">Stream name</label>
-    <div class="control">
-      <input v-model="name" class="input" type="text" placeholder="Jaguar 1">
+    <div class="notification" v-show="error">
+      <button class="delete" @click="onCloseAlert()"></button>
+      {{ error }}
     </div>
-  </div>
-  <div class="field field-stream-name">
-    <label for="location" class="label">Location</label>
-    <Map @locationSelected="onSelectLocation"></Map>
-  </div>
-  <div class="field is-grouped">
-    <p class="control control-btn">
-      <router-link class="control-btn" to="/"><button type="button" class="button is-rounded cancel">Cancel</button></router-link>
-    </p>
-    <p class="control control-btn">
-      <button type="button" class="button is-rounded is-primary" :class="{ 'is-loading': isLoading }" :disabled="!hasPassedValidation" @click.prevent="createStream">Create</button>
-    </p>
-  </div>
-</fieldset>
+    <div class="field field-stream-name">
+      <label for="name" class="label">Stream name</label>
+      <div class="control">
+        <input v-model="name" class="input" type="text" placeholder="Jaguar 1" />
+      </div>
+    </div>
+    <div class="field field-stream-name">
+      <label for="location" class="label">Location</label>
+      <Map @locationSelected="onSelectLocation"></Map>
+    </div>
+    <div class="field is-grouped">
+      <p class="control control-btn">
+        <router-link class="control-btn" to="/">
+          <button type="button" class="button is-rounded cancel">Cancel</button>
+        </router-link>
+      </p>
+      <p class="control control-btn">
+        <button
+          type="button"
+          class="button is-rounded is-primary"
+          :class="{ 'is-loading': isLoading }"
+          :disabled="!hasPassedValidation"
+          @click.prevent="createStream"
+        >Create</button>
+      </p>
+    </div>
+  </fieldset>
 </template>
 
 <script>
@@ -45,7 +53,9 @@ export default {
   components: { Map },
   computed: {
     hasPassedValidation () {
-      if (!(this.name && this.selectedLatitude && this.selectedLongitude)) return false
+      if (!(this.name && this.selectedLatitude && this.selectedLongitude)) {
+        return false
+      }
       return true
     }
   },
@@ -63,26 +73,38 @@ export default {
       let listener = (event, arg) => {
         this.$electron.ipcRenderer.removeListener('sendIdToken', listener)
         let idToken = arg
-        api.createStream(this.isProductionEnv(), this.name, latitude, longitude, visibility, idToken).then(async streamId => {
-          const stream = {
-            id: streamId,
-            name: this.name,
-            latitude: latitude,
-            longitude: longitude,
-            env: this.isProductionEnv() ? 'production' : 'staging',
-            visibility: visibility
-          }
-          console.log('creating stream', JSON.stringify(stream))
-          Stream.insert({ data: stream, insert: ['files'] })
-          this.$store.dispatch('setSelectedStreamId', stream.id)
-          this.$router.push('/')
-        }).catch(error => {
-          console.log('error while creating stream', error)
-          this.isLoading = false
-          if (error.status === 401 || error.data === 'UNAUTHORIZED') {
-            this.error = 'You are not authorized.'
-          } else { this.error = error.message || error.data }
-        })
+        api
+          .createStream(
+            this.isProductionEnv(),
+            this.name,
+            latitude,
+            longitude,
+            visibility,
+            idToken
+          )
+          .then(async streamId => {
+            const stream = {
+              id: streamId,
+              name: this.name,
+              latitude: latitude,
+              longitude: longitude,
+              env: this.isProductionEnv() ? 'production' : 'staging',
+              visibility: visibility
+            }
+            console.log('creating stream', JSON.stringify(stream))
+            Stream.insert({ data: stream, insert: ['files'] })
+            this.$store.dispatch('setSelectedStreamId', stream.id)
+            this.$router.push('/')
+          })
+          .catch(error => {
+            console.log('error while creating stream', error)
+            this.isLoading = false
+            if (error.status === 401 || error.data === 'UNAUTHORIZED') {
+              this.error = 'You are not authorized.'
+            } else {
+              this.error = error.message || error.data
+            }
+          })
       }
       this.isLoading = true
       this.$electron.ipcRenderer.send('getIdToken')
@@ -96,11 +118,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .fieldset {
-   &__wrapper {
-      margin: 32px auto;
-      padding: 16px;
-      max-width: 500px;
-    }
+.fieldset {
+  &__wrapper {
+    margin: 32px auto;
+    padding: 16px;
+    max-width: 500px;
   }
+}
 </style>
