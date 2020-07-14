d5e13ba77dec676481f118c3a194c8124c9653cf
Somjintana Korbut <nuisomjin@gmail.com>
2020-07-14 20:56:24 +0700

    WB-1563 update empty page for no stream case

--- src/renderer/components/LandingPage/EmptyStream.vue
+++ src/renderer/components/LandingPage/EmptyStream.vue
@@ -1,7 +1,23 @@
 <template>
     <div class="container-box empty has-text-centered">
-        <img src="~@/assets/ic-folder.svg" style="margin-bottom: 0.75em"><br>
-        <span>A stream is a group of audio files. Drop folders here to create new streams or use </span>
-        <img src="~@/assets/ic-add.svg"><span> button to create your first stream and start uploading files.</span><br>
+        <img src="~@/assets/ic-file.svg" style="margin-bottom: 0.75em"><br>
+        <p>A stream is a group of audio files.</p>
+        <span>Use  <img src="~@/assets/ic-add-stream.svg" style="vertical-align: sub"/>  button to create your first stream and start uploading files.</span><br>
     </div>
 </template>
+
+<style lang="scss" scoped>
+
+  span {
+    width: 100px;
+    text-align: center;
+  }
+
+  .container-box.empty {
+    margin: 16px auto;
+    padding: 16px;
+    max-width: 300px;
+    transform: translateY(50%);
+  }
+  
+</style>
