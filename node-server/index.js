d29f58a2f5c51f48b6aa7b3f2161ee7bd424c7c3
Somjintana Korbut <nuisomjin@gmail.com>
2020-07-22 22:55:36 +0700

    Add build script for windows on mac

--- node-server/index.js
+++ node-server/index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
-app.use(express.static('public'));
+app.use(express.static('.'))
 
-app.listen(5000, () => console.log('Listening on port 5000'));
+app.listen(5000, () => console.log('Listening on port 5000'))
