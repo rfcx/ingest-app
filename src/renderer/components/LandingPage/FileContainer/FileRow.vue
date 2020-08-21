39d8d3a302b9738f83c1a225517476d6ec1417a7
Phongnarin Sornroong <dendev.phongnarin@gmail.com>
2020-08-21 16:46:16 +0700

    WB-1566 - Create function to rename file on disk - Update file name to Stream & File db - Add file edit form

--- src/renderer/components/LandingPage/FileContainer/FileRow.vue
+++ src/renderer/components/LandingPage/FileContainer/FileRow.vue
@@ -4,8 +4,38 @@
       <img :class="{ 'file-failed': file.isError }" :src="getStateImgUrl(file.state)">
       <span class="file-status-state">{{ getStateName(file) }}</span>
     </td>
-    <td class="file-row file-list-table__cell file-list-table__cell_name" :class="{ 'is-error': file.isError }" >
-      {{ file.name }}
+    <td class="file-row file-list-table__cell file-list-table__cell_name" :class="{ 'is-error': file.isError, 'is-editing': isEdit }" >
+      <div class="is-flex flex-row filename-content">
+        <template v-if="isEdit">
+          <!-- filename input -->
+          <div class="file-name-input-container">
+            <input @keypress="onInputKeyPress($event)" :disabled="isDisabled" ref="inputFileName" v-model="fileName" type="text" placeholder="File name" class="input file-name-input" />
+          </div>
+
+          <!-- cancel edit button -->
+          <button
+            class="button is-rounded cancel-edit-filename-btn is-cancel"
+            @click="isEdit = false"
+            :disabled="isDisabled">
+            Cancel
+          </button>
+
+          <!-- save filename button -->
+          <button
+            class="button is-rounded save-edit-filename-btn is-primary"
+            :disabled="isDisabled || !canSave || !fileExtensionValid"
+            @click="saveEditFileName()"
+            :class="{'is-loading': loading }">
+            Save
+          </button>
+        </template>
+        <template v-else>
+          <div class="cell-file-name" v-text="file.name" />
+          <button class="button edit-file-name-btn" @click="editClick()" title="Edit file name">
+            <fa-icon class="icon-redo" :icon="icons.edit" />
+          </button>
+        </template>
+      </div>
     </td>
     <td class="file-row file-list-table__cell file-list-table__cell_info" v-if="!file.isError">
       {{ getTimestamp(file) }}
@@ -20,8 +50,8 @@
       {{ file.stateMessage }}
     </td>
     <td class="file-row file-row-icons file-list-table__cell file-list-table__cell_controls">
-      <font-awesome-icon v-if="file.canRedo" class="iconRedo" :icon="iconRedo" @click="repeatUploading(file)"></font-awesome-icon>
-      <font-awesome-icon v-if="file.canRemove" class="iconTrash" :icon="iconTrash" @click="remove(file)"></font-awesome-icon>
+      <fa-icon v-if="file.canRedo" class="icon-redo" :icon="icons.redo" @click="repeatUploading(file)" />
+      <fa-icon v-if="file.canRemove" class="icon-trash" :icon="icons.trash" @click="remove(file)" />
     </td>
   </tr>
 </template>
@@ -31,20 +61,53 @@ import File from '../../../store/models/File'
 import fileState from '../../../../../utils/fileState'
 import dateHelper from '../../../../../utils/dateHelper'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faRedo, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faRedo, faTrash, faPencilAlt } from '@fortawesome/free-solid-svg-icons'
 
 export default {
-  data () {
-    return {
-      iconRedo: faRedo,
-      iconTrash: faTrash
-    }
-  },
   props: {
     file: File
   },
-  components: { FontAwesomeIcon },
+  data: () => ({
+    isEdit: false,
+    fileName: '',
+    loading: false
+  }),
+  components: {
+    'fa-icon': FontAwesomeIcon
+  },
+  mounted () {
+    this.fileName = this.file.name || ''
+  },
   methods: {
+    onInputKeyPress (e) {
+      // trigger update when key 'Enter' presses
+      if (e.keyCode === 13) {
+        e.preventDefault()
+        if (this.canSave && this.fileExtensionValid) {
+          this.saveEditFileName()
+        }
+      }
+    },
+    async saveEditFileName () {
+      this.loading = true
+      try {
+        const newFilename = this.fileName.trim()
+        await this.$file.renameFile(this.file, newFilename)
+        this.isEdit = false
+      } catch (e) {
+        console.log('Save file name error', e)
+        // TODO: notify error
+      }
+      this.loading = false
+    },
+    editClick () {
+      this.fileName = this.file.name || ''
+      this.isEdit = true
+      setTimeout(() => {
+        const input = this.$refs.inputFileName
+        input && input.focus()
+      }, 50)
+    },
     getStateName (file) {
       return fileState.getName(file.state, file.stateMessage)
     },
@@ -67,6 +130,27 @@ export default {
       if (!file.canRemove) return
       File.delete(file.id)
     }
+  },
+  computed: {
+    icons: () => ({
+      redo: faRedo,
+      trash: faTrash,
+      edit: faPencilAlt
+    }),
+    isDisabled () {
+      return this.loading
+    },
+    canSave () {
+      const fname = this.fileName.trim()
+      return !this.loading && fname !== '' && fname !== this.file.name
+    },
+    fileExtensionValid () {
+      const arr = this.fileName.trim().split('.')
+      if (arr.length <= 1) {
+        return false
+      }
+      return String(this.file.extension).toLowerCase() === String(arr[arr.length - 1]).toLowerCase()
+    }
   }
 }
 </script>
@@ -77,13 +161,67 @@ export default {
     color: $secondary-text-color;
   }
 
-  .iconRedo {
+  .icon-redo {
     color: #ffffff;
     font-size: 13px;
     cursor: pointer;
   }
-  .iconTrash {
+  .icon-trash {
     margin-left: 4px;
     cursor: pointer;
+  }
+
+  .flex-row {
+    flex-direction: row;
+  }
+
+  .file-list-table__cell_name {
+    &:hover {
+      .edit-file-name-btn {
+        display: flex;
+      }
+    }
+
+    .file-name-input-container {
+      flex-grow: 1;
+      min-width: 40px;
+      max-width: 320px;
+      input {
+        width: 100%;
+        &:hover {
+          border-color: transparent;
+        }
+
+        &[disabled] {
+          background: #232436;
+          border-color: #232436;
+        }
+      }
+    }
+
+    .edit-file-name-btn {
+      margin-left: 8px;
+      background-color: transparent;
+      height: 20px;
+      width: 20px;
+      border: 0 !important;
+      box-shadow: unset !important ;
+      display: none;
+    }
+
+    .save-edit-filename-btn, .cancel-edit-filename-btn {
+      margin-left: 8px;
+      color: #ffffff;
+      min-width: 90px;
+    }
+
+    .cancel-edit-filename-btn[disabled] {
+      background: #232436;
+      border-color: #232436;
+    }
+  }
+
+  .filename-content {
+    align-items: center;
   }
 </style>
