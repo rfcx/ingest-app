3529f9df1e613bd8955eb44a5a36e00670f0dbb1
Somjintana Korbut <nuisomjin@gmail.com>
2020-08-31 11:14:27 +0700

    WB-2002 Add delete functionality to Edit screen

--- src/renderer/components/EditStreamLocation/EditStreamLocationPage.vue
+++ src/renderer/components/EditStreamLocation/EditStreamLocationPage.vue
@@ -16,25 +16,44 @@
         <label for="location" class="label">Location</label>
         <Map class="map-wrapper" @locationSelected="onSelectLocation" :lngLat="getStreamCoordinates()"></Map>
       </div>
-      <div class="field is-grouped">
-        <p class="control control-btn">
-          <router-link class="control-btn" to="/">
-            <button type="button" class="button is-rounded is-cancel">Cancel</button>
-          </router-link>
-        </p>
-        <p class="control control-btn">
-          <button type="button" class="button is-rounded is-primary" :class="{ 'is-loading': isLoading }" :disabled="!(hasEditedData && isNewStreamNameValid)" @click.prevent="updateStream">Apply</button>
-        </p>
+      <div class="buttons__wrapper">
+        <div class="field is-grouped">
+          <p class="control control-btn">
+            <router-link class="control-btn" to="/">
+              <button type="button" class="button is-rounded is-cancel">Cancel</button>
+            </router-link>
+          </p>
+          <p class="control control-btn">
+            <button type="button" class="button is-rounded is-primary" :class="{ 'is-loading': isLoading }" :disabled="!(hasEditedData && isNewStreamNameValid)" @click.prevent="updateStream">Apply</button>
+          </p>
+        </div>
+        <button type="button" class="button is-danger is-rounded is-outlined" :class="{'is-loading': isDeleting}" @click.prevent="showConfirmToDeleteStreamModal"><font-awesome-icon class="iconTrash" :icon="iconTrash"></font-awesome-icon> Delete Site</button>
       </div>
     </fieldset>
+    <!-- Modal -->
+    <div class="modal alert" :class="{ 'is-active': shouldShowConfirmToDeleteModal }">
+      <div class="modal-background"></div>
+      <div class="modal-card">
+        <div class="modal-card-body">
+          <p class="modal-card-title">Are you sure you want to delete this site?</p>
+        </div>
+        <footer class="modal-card-foot">
+          <button class="button is-rounded" @click="hideConfirmToDeleteStreamModal()">Cancel</button>
+          <button class="button is-danger is-rounded" :class="{ 'is-loading': isDeleting }" @click.prevent="deleteStream()">Delete</button>
+        </footer>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import Stream from '../../store/models/Stream'
+import File from '../../store/models/File'
 import api from '../../../../utils/api'
 import settings from 'electron-settings'
 import Map from '../CreateStream/Map'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { mapState } from 'vuex'
 
 export default {
   data () {
@@ -43,14 +62,18 @@ export default {
       selectedLatitude: null,
       selectedLongitude: null,
       isLoading: false,
-      error: ''
+      isDeleting: false,
+      error: '',
+      iconTrash: faTrash,
+      shouldShowConfirmToDeleteModal: false
     }
   },
   components: { Map },
   computed: {
-    selectedStreamId () {
-      return this.$store.state.Stream.selectedStreamId
-    },
+    ...mapState({
+      selectedStreamId: state => state.Stream.selectedStreamId,
+      currentUploadingSessionId: state => state.AppSetting.currentUploadingSessionId
+    }),
     selectedStream () {
       return Stream.find(this.selectedStreamId)
     },
@@ -87,7 +110,7 @@ export default {
               data: { latitude: latitude, longitude: longitude, name: name }
             })
             this.isLoading = false
-            this.$router.push('/')
+            this.redirectToMainScreen()
           })
           .catch(error => {
             console.log('error while updating stream coordinates', error)
@@ -104,6 +127,77 @@ export default {
     },
     getStreamCoordinates () {
       return this.selectedStream.longitude ? [this.selectedStream.longitude, this.selectedStream.latitude] : null
+    },
+    showConfirmToDeleteStreamModal () {
+      this.shouldShowConfirmToDeleteModal = true
+    },
+    hideConfirmToDeleteStreamModal () {
+      this.shouldShowConfirmToDeleteModal = false
+    },
+    deleteStream () {
+      this.error = null
+      this.isDeleting = true
+      let listener = (event, arg) => {
+        this.$electron.ipcRenderer.removeListener('sendIdToken', listener)
+        let idToken = null
+        idToken = arg
+        api.deleteStream(this.isProductionEnv(), this.selectedStream.id, idToken).then(async (data) => {
+          console.log('stream is deleted')
+          await this.removeStreamFromVuex(this.selectedStream.id)
+          this.modalHandler()
+        }).catch(error => {
+          console.log('error while deleting stream', error)
+          this.errorHandler(error, true)
+        })
+      }
+      this.$electron.ipcRenderer.send('getIdToken')
+      this.$electron.ipcRenderer.on('sendIdToken', listener)
+    },
+    async removeStreamFromVuex (selectedStreamId) {
+      let ids = File.query().where('streamId', this.selectedStreamId).get().map((file) => { return file.id })
+      if (ids && ids.length > 0) {
+        let listen = (event, arg) => {
+          this.$electron.ipcRenderer.removeListener('filesDeleted', listen)
+          console.log('files deleted')
+          Stream.delete(selectedStreamId)
+        }
+        this.$electron.ipcRenderer.send('deleteFiles', ids)
+        this.$electron.ipcRenderer.on('filesDeleted', listen)
+      } else {
+        Stream.delete(selectedStreamId)
+      }
+    },
+    modalHandler () {
+      const stream = Stream.query().where((stream) => {
+        return stream.id !== this.selectedStreamId
+      }).first()
+      if (stream) {
+        this.$store.dispatch('setSelectedStreamId', stream.id)
+      }
+      // If a stream deleted when the uploading process was paused.
+      const files = File.query().where('sessionId', this.currentUploadingSessionId).get()
+      files.forEach(file => {
+        File.update({ where: file.id,
+          data: { paused: false }
+        })
+      })
+      this.isDeleting = false
+      this.redirectToMainScreen()
+    },
+    errorHandler (error, isDeleting) {
+      this.isDeleting = false
+      this.hideConfirmToDeleteStreamModal()
+      setTimeout(() => {
+        this.error = null
+      }, 10000)
+      if (error.status === 401 || error.data === 'UNAUTHORIZED') {
+        this.error = 'You are not authorized.'
+      } else if (error.status === 403) {
+        this.error = `You don't have permissions to delete non-empty stream.`
+      } else { this.error = 'Error while deleting stream.' }
+    },
+    redirectToMainScreen () {
+      this.$router.push('/')
     }
   },
   mounted () {
@@ -127,6 +221,15 @@ export default {
   .mapboxgl-canvas {
     height: 300px !important;
   }
+}
+.buttons {
+  &__wrapper {
+    display: flex;
+    justify-content: space-between;
+  }
+}
+.iconTrash {
+  margin-right: 4px;
 }
 .notification {
   background: #3b3e53 !important;
