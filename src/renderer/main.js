5949d331f1540d70e6bbcd7419e798fdc3361657
Somjintana Korbut <nuisomjin@gmail.com>
2020-11-25 22:06:12 +0700

    PI-141 Integrate firebase performance monitoring

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -6,10 +6,24 @@ import App from './App'
 import router from './router'
 import store from './store'
 import file from './services/file'
+import firebase from 'firebase/app'
+import 'firebase/performance'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faChevronUp, faChevronDown, faPencilAlt, faRedo, faEyeSlash, faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCfEDNKPWom20xfv69v-YHuT1XzwPg8B_g',
+  authDomain: 'rfcx-ingest-257610.firebaseapp.com',
+  databaseURL: 'https://rfcx-ingest-257610.firebaseio.com',
+  projectId: 'rfcx-ingest-257610',
+  storageBucket: 'rfcx-ingest-257610.appspot.com',
+  messagingSenderId: '245204209562',
+  appId: '1:245204209562:web:93bcf78069d63042463423'
+}
+firebase.initializeApp(firebaseConfig)
+firebase.performance()
 
 library.add(faChevronUp, faChevronDown, faPencilAlt, faRedo, faEyeSlash, faExternalLinkAlt)
 
