e0cb7cc052c23f18f76f5a93555c7156ed8e3ddd
Somjintana Korbut <nuisomjin@gmail.com>
2021-02-26 11:57:58 +0700

    PI-598 add stream id into converted tmp filename for uniqueness

--- utils/api.js
+++ utils/api.js
@@ -42,7 +42,7 @@ const uploadFile = async (environment, fileId, fileName, filePath, fileExt, stre
       File.update({ where: fileId,
         data: {state: 'converting', uploaded: false}
       })
-      uploadFilePath = (await fileHelper.convert(filePath, remote.app.getPath('temp'))).path
+      uploadFilePath = (await fileHelper.convert(filePath, remote.app.getPath('temp'), streamId)).path
       uploadFileExt = 'flac'
       uploadFileName = fileHelper.getFileNameFromFilePath(uploadFilePath)
       isConverted = true
