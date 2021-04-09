4ba5fb799570ab61c9bf2937a8db1af6f4dbc458
Somjintana Korbut <nuisomjin@gmail.com>
2021-04-09 14:05:30 +0700

    CE-463 fix the global progress didn't show up when the uploading is paused

--- src/renderer/components/SideNavigation/GlobalProgress.vue
+++ src/renderer/components/SideNavigation/GlobalProgress.vue
@@ -117,6 +117,7 @@ export default {
     }
   },
   created () {
+    this.getStats()
     this.refreshInterval = setInterval(() => {
       if (this.isUploadingProcessEnabled) {
         this.getStats()
