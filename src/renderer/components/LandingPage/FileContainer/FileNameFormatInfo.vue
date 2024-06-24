bdc6625de9f48cdb7050a47d701393a7655c5d7d
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2024-06-24 22:11:45 +0400

    #228 Fix the timezone issue for UTC

--- src/renderer/components/LandingPage/FileContainer/FileNameFormatInfo.vue
+++ src/renderer/components/LandingPage/FileContainer/FileNameFormatInfo.vue
@@ -167,7 +167,7 @@ export default {
           }
         }
       } else {
-        bulkUpdateParams.timezone = timezone
+        bulkUpdateParams.values.timezone = timezone
         await ipcRendererSend('db.files.bulkUpdate', `db.files.bulkUpdate.${Date.now()}`, bulkUpdateParams)
       }
       console.info('[queueToUpload] selected timezone', timezone)
